903d705adf9afd901ff9c56b2b7d609aeca8832f
santiago <3014593200101@ingenieria.usac.edu.gt>
2025-03-31 22:54:20 -0600

    Creación del proyecto

---
+++ src/main/java/com/grupo6/mc1/proyecto1/MC1Proyecto1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo6.mc1.proyecto1;
+
+/**
+ *
+ * @author José Santiago Herrera Furlán - 202403244
+ * @author Alysson Gabriela Benito Ixcoy - 202400225
+ */
+public class MC1Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
